8d195a0e57aa92690373a2abe1090ed9173cc6e0
Hidehiro Aya <hi.dehiro424@gmail.com>
2020-12-23 00:31:56 +0900

    production_db_urlの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::API
     def hello_world
         render json:{text:"Hello World"}
     end
+
+    def test
+        render json:{text:"Hello"}
+    end
+
 end
