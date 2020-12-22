bb1f0c170df2e7d902253a37da8507a8496a8851
Hidehiro Aya <hi.dehiro424@gmail.com>
2020-12-22 18:22:58 +0900

    get/to:の理解とroutes.rbの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :owners, only:[:index]
-  resources :properties, only:[:index]
+  get "hello_world", to: 'application#hello_world'
+  get "owners", to: 'owners#index'
+  get "properties", to: 'properties#index'
+  
+  # resources :owners, only:[:index]
+  # resources :properties, only:[:index]
 end
